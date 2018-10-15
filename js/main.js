52bd2efa2873ddd2fa8c2493619247a29ca31011
Angelo Johnson <42251738+angeloplacebo@users.noreply.github.com>
2018-10-14 21:22:02 -0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+ScrollReveal().reveal('.showup',{delay: 500});
+ScrollReveal().reveal('.showuptext',{delay: 700});
+ScrollReveal().reveal('.showuptext2',{delay: 900});
+
+$(function () {
+    $('[data-toggle="popover"]').popover()
+  })
